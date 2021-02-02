2387ab7447468f17e566716a9e7c7d41dfecd1e4
UdayKiranBayya <74950994+UdayKiranBayya@users.noreply.github.com>
2021-02-02 19:30:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,31 +17,31 @@ for(var i = 0; i <= 6; i++){
 //function that play sounds..
 function Click(button){
     if (button === "w"){
-       var audio = new Audio('sounds/crash.mp3');
+       var audio = new Audio('crash.mp3');
        audio.play();
     }
     else if (button === "a"){
-       var audio = new Audio('sounds/kick-bass.mp3');
+       var audio = new Audio('kick-bass.mp3');
        audio.play();
     }
     else if (button === "s"){
-       var audio = new Audio('sounds/snare.mp3');
+       var audio = new Audio('snare.mp3');
        audio.play();
     }
     else if (button === "d"){
-       var audio = new Audio('sounds/tom-1.mp3');
+       var audio = new Audio('tom-1.mp3');
        audio.play();
     }
     else if (button === "j"){
-       var audio = new Audio('sounds/tom-2.mp3');
+       var audio = new Audio('tom-2.mp3');
        audio.play();
     }
     else if (button === "k"){
-       var audio = new Audio('sounds/tom-3.mp3');
+       var audio = new Audio('tom-3.mp3');
        audio.play();
     }
     else if (button === "l"){
-       var audio = new Audio('sounds/crash.mp3');
+       var audio = new Audio('tom-4.mp3');
        audio.play();
     }
 
