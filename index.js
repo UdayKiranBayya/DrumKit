7512a6691cb9a4fc9038d3b94e8eac5f80d5a836
UdayKiranBayya <74950994+UdayKiranBayya@users.noreply.github.com>
2021-02-02 19:28:57 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+//looping over all the buttons..
+for(var i = 0; i <= 6; i++){
+//listening to mouse clicks..
+    document.querySelectorAll('button')[i].addEventListener('click', function(){
+    var btn = this.innerHTML;
+    Click(btn);
+    animation(btn);
+})
+//listening to keyboard button clicks..
+    var buttonPress = addEventListener("keydown", function(event){
+        var btnx = event.key;
+        Click(btnx);
+        animation(btnx);
+    })
+}
+
+//function that play sounds..
+function Click(button){
+    if (button === "w"){
+       var audio = new Audio('sounds/crash.mp3');
+       audio.play();
+    }
+    else if (button === "a"){
+       var audio = new Audio('sounds/kick-bass.mp3');
+       audio.play();
+    }
+    else if (button === "s"){
+       var audio = new Audio('sounds/snare.mp3');
+       audio.play();
+    }
+    else if (button === "d"){
+       var audio = new Audio('sounds/tom-1.mp3');
+       audio.play();
+    }
+    else if (button === "j"){
+       var audio = new Audio('sounds/tom-2.mp3');
+       audio.play();
+    }
+    else if (button === "k"){
+       var audio = new Audio('sounds/tom-3.mp3');
+       audio.play();
+    }
+    else if (button === "l"){
+       var audio = new Audio('sounds/crash.mp3');
+       audio.play();
+    }
+
+
+
+}
+function animation(button){
+    var toBeAnimated = document.querySelector("." + button);
+    toBeAnimated.classList.add("pressed");
+    setTimeout(function(){
+        toBeAnimated.classList.remove("pressed");
+
+    }, 100)
+
+}
+
+
+
